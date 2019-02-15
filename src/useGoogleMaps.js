42ef880f028fc7e0d534b9cfc2e8728896a6006a
Austin Hunt <austin@hailtrace.com>
2019-02-15 18:42:57 -0500

    Added support for maps api hook.

---
+++ src/useGoogleMaps.js
@@ -0,0 +1,27 @@
+import React from 'react';
+const mapsapi = require('google-maps-api');
+
+export const getMapsApi = (options, callback) => {
+  if (!options.libraries) console.warn(`Please provide a list of libraries to include in the maps api.`);
+  if (!options.apiKey) throw new Error('Api key must be provided.');
+
+  mapsapi(options.apiKey, options.version, options.libraries || [])()
+    .then(
+      maps => setMaps(maps)
+    )
+    .catch(error => log.error(error));
+}
+
+export default function useGoogleMaps({ apiKey, version, libaries }) {
+  const [ maps, setMaps ] = React.useState(null);
+
+  React.useEffect(() => {
+    if (apiKey) {
+      getMapsApi({ apiKey, version, libraries }, maps => setMaps(maps));
+    }
+  }, [
+    apiKey
+  ]);
+
+  return maps;
+}
